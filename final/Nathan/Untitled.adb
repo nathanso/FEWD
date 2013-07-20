6501856213e0ae07450b53634b8f5b96579975a5
nathanso <nathan.so.96@gmail.com>
2013-07-20 21:24:25 +0800

    lol

---
+++ final/Nathan/Untitled.adb
@@ -0,0 +1,16 @@
+CSS things
+
+in HTML, you can define something a different class
+
+Eg:
+
+< p classtype="Something">…….
+
+
+in css, you can define the thing easily
+
+(p and classtype are two different things)
+
+so…
+p{…}
+.something{…}(quite important)
