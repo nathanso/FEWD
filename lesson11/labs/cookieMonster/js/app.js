d3a3a72977e68210d855ed5f1b21101eefeb01f1
Nathan So <nathan.so.96@gmail.com>
2013-08-22 17:57:48 +0800

    NO MORE BUSY hANDS

---
+++ lesson11/labs/cookieMonster/js/app.js
@@ -0,0 +1,11 @@
+	var counter = 1;
+
+$(document).ready(function(){
+    $('#startSwapping').click(function(){
+		counter=counter + 1;
+		$('body').css('background-image','url(images/bg' + (counter % 4 + 1) +'.jpg)');
+		// $('body').css('background-image','url(images/bg1.jpg)');
+		// $('body').css('color', 'red');
+	})
+})
+
