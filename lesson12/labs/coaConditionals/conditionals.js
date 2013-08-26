ed36d75871fdd491c536b728fc5de8a3c03390c3
Nathan So <nathan.so.96@gmail.com>
2013-08-26 12:18:05 +0800

    jkln;j

--- lesson12/labs/coaConditionals/conditionals.js
+++ lesson12/labs/coaConditionals/conditionals.js
@@ -5,24 +5,40 @@
 
 	 //Prompt the user for their name and last name. 
 
+var name = prompt ("What is your name (not including last Name)?")
+var lastname = prompt ("What is your last name?")
+
 
 	//Create a new variable called full name and store the users full name.
 	
+var fullname = name + " " + lastname	
 	
 	//Print the full name to the console.
-	
+
+console.log (fullname);
 
 	//Prompt the user for their age.
 	
-
+var age = prompt ("What is your age?");
 
 	//Add 10 to the age and print the output to the console.
 	
-	
+console.log (10 + parseInt(age));	
 	
 	//Verify that the user is over 18 and print if they are a minor or adult to the console.
+age = 10 + parseInt(age);
 
-
+if (age >= 18){
+	console.log ("Adult");
+}
+else {
+	console.log ("Minor");
+}
 	
 	//Verify if the first name is "General" and the last name is NOT "Assembly"
 
+if (name == "General" && lastname != "Assembly"){
+	console.log ("True");
+}else {
+	console.log ("False");	
+}
--- lesson12/labs/lineByLine/line_by_line.js
+++ lesson12/labs/lineByLine/line_by_line.js
@@ -2,3 +2,14 @@
 Write three statements that prints your first name, 
 last name and your favorite programming language thus far to the console.
 */  
+
+console.log ("Nathan So Java");
+console.log ("So");
+console.log ("Java");
+
+var reply = prompt("What's your Name?");
+
+If (reply.length === 6);
+
+(Prompt ("you are Awesome")
+	)
